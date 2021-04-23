8b9766a051acf56e7bbf28640dd8fb647ddc59bc
Giovandro <giovandrofabiosantos@hotmail.com>
2021-04-23 10:35:28 -0300

    Classe JpaUsuario

--- fj21-tarefas/src/main/java/br/com/caelum/tarefas/modelo/Usuario.java
+++ fj21-tarefas/src/main/java/br/com/caelum/tarefas/modelo/Usuario.java
@@ -1,9 +1,27 @@
 package br.com.caelum.tarefas.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy	=	GenerationType.IDENTITY)
+	private Long id;
 	private String login;
 	private String senha;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getLogin() {
 		return login;
 	}
